b9aed18f8a5ec8f83363f93c7cbe5572d8aa18fc
oss6 <ossedb@gmail.com>
2018-03-14 11:51:26 +0000

    Add breakpoints for dfa min

--- src/components/DiagTable/DiagTable.js
+++ src/components/DiagTable/DiagTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Icon, Table } from 'semantic-ui-react';
 import { For, Choose } from 'react-extras';
 import DataPlaceholder from 'components/DataPlaceholder/DataPlaceholder';
 import './DiagTable.css';
@@ -13,6 +13,7 @@ export default class DiagTable extends Component {
 
     row: (contents, cols) => (row, idx) => (
       <Table.Row key={idx}>
+        <Table.Cell>{cols[idx]}</Table.Cell>
         <For of={cols} render={this.renderers.col(contents, idx)}/>
       </Table.Row>
     ),
@@ -21,7 +22,7 @@ export default class DiagTable extends Component {
       <Table.Cell key={colIdx}>
         <Choose>
           <Choose.When condition={colIdx > rowIdx}>
-            {contents[rowIdx] ? contents[rowIdx][colIdx - (rowIdx + 1)] + '' : null}
+            {contents[rowIdx] ? this.renderers.cell(contents[rowIdx][colIdx - (rowIdx + 1)]) : null}
           </Choose.When>
 
           <Choose.Otherwise disabled>
@@ -29,7 +30,9 @@ export default class DiagTable extends Component {
           </Choose.Otherwise>
         </Choose>
       </Table.Cell>
-    )
+    ),
+
+    cell: value => value ? <Icon name='checkmark' /> : <Icon name='x' />
   }
 
   render() {
@@ -49,6 +52,7 @@ export default class DiagTable extends Component {
           <Table celled>
             <Table.Header>
               <Table.Row>
+                <Table.HeaderCell></Table.HeaderCell>
                 <For of={data ? data.cols : []} render={this.renderers.columnHeader}/>
               </Table.Row>
             </Table.Header>
--- src/containers/dfaMinimization/breakpoint.js
+++ src/containers/dfaMinimization/breakpoint.js
@@ -4,26 +4,9 @@ import _ from 'lodash';
 
 export const breakpoint = {};
 
-/**
- * Forward
- */
-
-breakpoint.forward = {};
-
-breakpoint.forward.reachableStates = function ({ data, index }) {
-
-  if (this.refs.actionsHistory) {
-    this.refs.actionsHistory.addOrSelect(index, {
-      title: `Reachable states: {${data.states.map(s => s.id).join(', ')}}`,
-      breakpoint: data
-    });
-  }
-};
-
-breakpoint.forward.initialTable = function ({ data, index }) {
-
-  const cols = data.table.cols.map(c => c.id);
-  const rows = data.table.rows.map(r => r.id);
+const updateTable = function (data) {
+  const cols = data.table.cols.map(c => c.id === -1 ? 'Error state' : c.id);
+  const rows = data.table.rows.map(r => r.id === -1 ? 'Error state' : r.id);
 
   this.setState({
     table: {
@@ -32,10 +15,90 @@ breakpoint.forward.initialTable = function ({ data, index }) {
       contents: data.table.contents
     }
   });
+};
+
+/**
+ * Forward
+ */
+
+breakpoint.forward = {};
+
+breakpoint.forward.inputDfa = function ({ data, index }) {
+  this.setState({
+    dfa: automata.visDataFormat('dfa-viz', data.dfa)
+  });
+
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: 'Consider the DFA to minimize',
+      breakpoint: data
+    });
+  }
+}
+
+breakpoint.forward.reachableStates = function ({ data, index }) {
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: `Reachable states: {${data.states.map(s => s.id === -1 ? 'Error state' : s.id).join(', ')}}`,
+      breakpoint: data
+    });
+  }
+};
+
+breakpoint.forward.initialTable = function ({ data, index }) {
+  updateTable.call(this, data);
 
   if (this.refs.actionsHistory) {
     this.refs.actionsHistory.addOrSelect(index, {
       title: 'Initial table contents',
+      breakpoint: data
+    });
+  }
+};
+
+breakpoint.forward.updatedTable = function ({ data, index }) {
+  updateTable.call(this, data);
+
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: 'Update table contents',
+      breakpoint: data
+    });
+  }
+};
+
+breakpoint.forward.unmarkedStates = function ({ data, index }) {
+  const { states } = data;
+
+  for (const pair of states) {
+
+  }
+
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: states.length === 0 ? 'There are no states to merge.' : 'Unmarked pair of states have been highlighted.',
+      breakpoint: data
+    });
+  }
+};
+
+breakpoint.forward.updatedDfa = function ({ data, index }) {
+  this.setState({
+    minDfa: automata.visDataFormat('min-dfa-viz', data.dfa)
+  });
+
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: 'Unmarked pairs have been merged.',
+      breakpoint: data
+    });
+  }
+};
+
+breakpoint.forward.finish = function ({ data, index }) {
+  if (this.refs.actionsHistory) {
+    this.refs.actionsHistory.addOrSelect(index, {
+      title: 'The minimization has finished.',
       breakpoint: data
     });
   }
@@ -58,7 +121,8 @@ breakpoint.visualizationStates.commit = function () {
     minDfa: {
       nodes: clone(this.state.minDfa.nodes),
       edges: clone(this.state.minDfa.edges)
-    }
+    },
+    table: clone(this.state.table)
   });
 
   this.setState({ visualizationStates });
@@ -95,6 +159,7 @@ breakpoint.visualizationStates.rollback = function () {
       nodes: minDfaNodes,
       edges: minDfaEdges
     },
+    table: clone(visualizationState.table),
     visualizationStates
   });
 };
